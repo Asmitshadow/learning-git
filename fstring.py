1c4fdc04e46814e1b35fadf60da7e7419c5fc700
Asmitshadow <rishistudent77@gmail.com>
2025-12-01 22:13:41 +0530

    some

---
+++ fstring.py
@@ -0,0 +1,15 @@
+"""s1 = "helllo"
+s2 = "world"
+s3 = "asmit"
+s4 = "raj"
+
+print(f"{s3} is a star of this {s2}")
+"""
+s1 = 32
+s2 = 45
+s3 = 43
+s4 = "asmit"
+add = s1 + s2 + s3
+percent = (s1 + s2 + s3) / 3
+print(f"this is the total marks {add} that taken by {s4}")
+print(f"{percent} of {s4} ")
--- input.py
+++ input.py
@@ -8,3 +8,11 @@ print(s3)
 print(s4)
 s7 = s1 +" "+ s2
 print(s7)
+
+
+s1 = int(input("num1 :"))
+s2 = int(input("num2 :"))
+
+add = s1 + s2 
+#s3 = input("addition : " , add)
+print(f"addition : {add}")
