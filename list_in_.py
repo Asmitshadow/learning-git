0e940f36f54cd83af5aa02bb3aec0939437cd298
Asmitshadow <rishistudent77@gmail.com>
2025-12-17 23:00:45 +0530

    wednesday

---
+++ list_in_.py
@@ -0,0 +1,18 @@
+# here we learn hoe to use list in python : we use [] that bracket for listing
+
+days_of_week = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+# let's do listing
+print(days_of_week)
+
+# length of days_of_week
+
+print(len(days_of_week))
+
+# let's try indexing
+
+print(days_of_week[0])
+print(days_of_week[-1])
+
+# let's use f
+
+print(f"this the {days_of_week[2]} third day of week")
